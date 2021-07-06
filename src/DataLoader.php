5bb30e18efe33742864952228a711c94ba5026a7
Joseph Rouff <rouffj@gmail.com>
2021-07-06 15:31:50 +0200

    Add doctrine:fixtures package

--- src/DataLoader.php
+++ src/DataLoader.php
@@ -9,12 +9,12 @@ class DataLoader
     public function getMovies(): array
     {
         return [
-            ['id' => 1, 'title' => 'Memento', 'releaseDate' => new \DateTime('2000-01-01'), 'plot' => 'Plot 1', 'image' => '/assets/images/movie-image-samples/memento.jpeg'],
-            ['id' => 2, 'title' => 'Insomnia', 'releaseDate' => new \DateTime('2002-03-04'), 'plot' => 'Plot 2', 'image' => '/assets/images/movie-image-samples/insomnia.jpeg'],
-            ['id' => 3, 'title' => 'The Dark Knight ', 'releaseDate' => new \DateTime('2008-05-01'), 'plot' => 'Plot 3', 'image' => '/assets/images/movie-image-samples/the-dark-knight.jpeg'],
-            ['id' => 4, 'title' => 'Inception', 'releaseDate' => new \DateTime('2010-02-05'), 'plot' => 'Plot 4', 'image' => '/assets/images/movie-image-samples/inception.jpeg'],
-            ['id' => 5, 'title' => 'Man Of Steel', 'releaseDate' => new \DateTime('2013-05-05'), 'plot' => 'Plot 5', 'image' => '/assets/images/movie-image-samples/man-of-steel.jpeg'],
-            ['id' => 6, 'title' => 'Dunkirk', 'releaseDate' => new \DateTime('2017-06-07'), 'plot' => 'Plot 6', 'image' => '/assets/images/movie-image-samples/dunkirk.jpeg'],
+            ['id' => 1, 'title' => 'Memento', 'duration' => 140, 'releaseDate' => new \DateTime('2000-01-01'), 'plot' => 'Plot 1', 'image' => '/assets/images/movie-image-samples/memento.jpeg'],
+            ['id' => 2, 'title' => 'Insomnia', 'duration' => 120, 'releaseDate' => new \DateTime('2002-03-04'), 'plot' => 'Plot 2', 'image' => '/assets/images/movie-image-samples/insomnia.jpeg'],
+            ['id' => 3, 'title' => 'The Dark Knight ', 'duration' => 150, 'releaseDate' => new \DateTime('2008-05-01'), 'plot' => 'Plot 3', 'image' => '/assets/images/movie-image-samples/the-dark-knight.jpeg'],
+            ['id' => 4, 'title' => 'Inception', 'duration' => 100, 'releaseDate' => new \DateTime('2010-02-05'), 'plot' => 'Plot 4', 'image' => '/assets/images/movie-image-samples/inception.jpeg'],
+            ['id' => 5, 'title' => 'Man Of Steel', 'duration' => 70, 'releaseDate' => new \DateTime('2013-05-05'), 'plot' => 'Plot 5', 'image' => '/assets/images/movie-image-samples/man-of-steel.jpeg'],
+            ['id' => 6, 'title' => 'Dunkirk', 'duration' => 110, 'releaseDate' => new \DateTime('2017-06-07'), 'plot' => 'Plot 6', 'image' => '/assets/images/movie-image-samples/dunkirk.jpeg'],
         ];
     }
 
